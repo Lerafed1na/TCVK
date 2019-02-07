1ade74915ba915647d8b63fd81675a8040fef346
Lerafed1na <lerasmewnaya@gmail.com>
2019-02-07 18:37:26 +0300

    Upload

---
+++ TinkoffChat/AppDelegate.swift
@@ -0,0 +1,143 @@
+//
+//  AppDelegate.swift
+//  TinkoffChat
+//
+//  Created by Lera Korenevich on 07/02/2019.
+//  Copyright © 2019 Lera Korenevich. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    var prevState = "Not running"
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Called if everything is fine with initialization
+       let state = convertState(state: UIApplication.shared.applicationState)
+       if state != prevState {
+            print("Application moved from \(prevState) to \(state): \(#function)")
+        } else {
+            print("Application is still in \(state): \(#function)")
+        }
+        prevState = state
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Switching to another application or pressing the "HOME" button
+        let state = convertState(state: UIApplication.shared.applicationState)
+        if state != prevState {
+            print("Application moved from \(prevState) to \(state): \(#function)")
+        } else {
+            print("Application is still in \(state): \(#function)")
+        }
+        prevState = state
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Transition to the background task execution state.
+        let state = convertState(state: UIApplication.shared.applicationState)
+        if state != prevState {
+            print("Application moved from \(prevState) to \(state): \(#function)")
+        } else {
+            print("Application is still in \(state): \(#function)")
+        }
+        prevState = state
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // The application has switched to the Foreground Inactive state. It was called from a minimized state and was in the background state.
+        let state = convertState(state: UIApplication.shared.applicationState)
+        if state != prevState {
+            print("Application moved from \(prevState) to \(state): \(#function)")
+        } else {
+            print("Application is still in \(state): \(#function)")
+        }
+        prevState = state
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // The application is in the Active state.
+        let state = convertState(state: UIApplication.shared.applicationState)
+        if state != prevState {
+            print("Application moved from \(prevState) to \(state): \(#function)")
+        } else {
+            print("Application is still in \(state): \(#function)")
+        }
+        prevState = state
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // When you close the application. Called only when the application is running. It is not called from the Suspended state.
+        let state = convertState(state: UIApplication.shared.applicationState)
+        if state != prevState {
+            print("Application moved from \(prevState) to \(state): \(#function)")
+        } else {
+            print("Application is still in \(state): \(#function)")
+        }
+        prevState = state
+        self.saveContext()
+    }
+    
+    func convertState(state: UIApplication.State) -> String {
+        switch state {
+        case .active:
+            return "active"
+        case .inactive:
+            return "inactive"
+        case .background:
+            return "background"
+        }
+    }
+
+    // MARK: - Core Data stack
+
+    lazy var persistentContainer: NSPersistentContainer = {
+        /*
+         The persistent container for the application. This implementation
+         creates and returns a container, having loaded the store for the
+         application to it. This property is optional since there are legitimate
+         error conditions that could cause the creation of the store to fail.
+        */
+        let container = NSPersistentContainer(name: "TinkoffChat")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                 
+                /*
+                 Typical reasons for an error here include:
+                 * The parent directory does not exist, cannot be created, or disallows writing.
+                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+                 * The device is out of space.
+                 * The store could not be migrated to the current model version.
+                 Check the error message to determine what the actual problem was.
+                 */
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+
+    // MARK: - Core Data Saving support
+
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+
+}
+
---
+++ TinkoffChat/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  TinkoffChat
+//
+//  Created by Lera Korenevich on 07/02/2019.
+//  Copyright © 2019 Lera Korenevich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        printStateName()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        printStateName()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+         printStateName()
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+         printStateName()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+         printStateName()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+         printStateName()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        printStateName()
+    }
+    
+    func printStateName(string: String = #function) {
+        print("\(string)")
+    }
+
+}
+
